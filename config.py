770e7b4ebd16ca0dca19d3afb327703498e67a17
Krishnasurya974 <krishnasurya974@gmail.com>
2024-02-21 19:45:51 +0530

    Made it deployment ready

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config:
+    SECRET_KEY = 'abc@123'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    #'sqlite:///events.db'
+    #postgresql://...
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'bxtj ifpy phii gyjp')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER', 'write2kvksurya@gmail.com')
+    API_KEY= os.environ.get('API_KEY')
